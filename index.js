6ca3a544c085d8b3399002ce1df6d7d1e69305a0
meghanctucker <meghanctucker@gmail.com>
2025-06-10 12:29:58 -0700

    Initial file transfer and upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//navigates welcome.html to welcomeSeq.html after animation completed
+document.getElementById("openWelcomeSeq").addEventListener("animationend", function() {
+    window.location.href = "welcomeSeq.html"; // Replace with your target URL
+});
+
+
+
